31e0b7a9533b1616646eea083c62374cbd639d41
BinadiSilva <binadisilva@gmail.com>
2026-04-09 16:18:21 +0530

    created-the-ticketing-backend

---
+++ smartcampusoperationshub/src/main/java/com/smartcampus/smartcampusoperationshub/controller/TicketController.java
@@ -0,0 +1,44 @@
+package com.smartcampus.smartcampusoperationshub.controller;
+
+import com.smartcampus.smartcampusoperationshub.model.Ticket;
+import com.smartcampus.smartcampusoperationshub.service.TicketService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tickets")
+@CrossOrigin(origins = "http://localhost:5173")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public Ticket createTicket(@RequestBody Ticket ticket) {
+        return ticketService.createTicket(ticket);
+    }
+
+    @GetMapping
+    public List<Ticket> getAllTickets() {
+        return ticketService.getAllTickets();
+    }
+
+    @GetMapping("/{id}")
+    public Ticket getTicketById(@PathVariable Long id) {
+        return ticketService.getTicketById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Ticket updateTicket(@PathVariable Long id, @RequestBody Ticket ticket) {
+        return ticketService.updateTicket(id, ticket);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTicket(@PathVariable Long id) {
+        ticketService.deleteTicket(id);
+    }
+}
